12c92dbf37d67c643fad711d44b8c438d931f646
Ngeny Gilbert <ngenygkip@gmail.com>
2024-08-09 23:08:58 +0300

    React hooks information flow lab

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React,{useState} from "react"
+
+const Header = ({onDarkModeClick}) => {
+    const [isDarkMode, setIsDarkMode] =useState(false)
+
+    const toggleDarkMode = () => {
+        setIsDarkMode((prevMode) => !prevMode)
+    }
+    return (
+        <header>
+            <h1>My App</h1>
+            <button onClick={onDarkModeClick}>Toggle Dark Mode</button>
+        </header>
+    );
+};
+export default Header;
